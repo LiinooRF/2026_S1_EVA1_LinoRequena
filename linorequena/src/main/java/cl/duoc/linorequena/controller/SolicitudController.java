0730bcadd550ff6b4063385ae9435ced3be0cec9
LiinooRF <linojotare0704@gmail.com>
2026-03-31 11:54:48 -0300

    Creación de service, sin responseentity

--- linorequena/src/main/java/cl/duoc/linorequena/controller/SolicitudController.java
+++ linorequena/src/main/java/cl/duoc/linorequena/controller/SolicitudController.java
@@ -1,8 +1,61 @@
 package cl.duoc.linorequena.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cl.duoc.linorequena.model.Solicitud;
+import cl.duoc.linorequena.service.SolicitudService;
 
 @Service
+@RequestMapping("api/solicitudes")
 public class SolicitudController {
+  
+    private final SolicitudService solicitudService;
+
+    public SolicitudController(SolicitudService solicitudService) {
+        this.solicitudService = solicitudService;
+    }
+
+    //Consultar solicitudes ya almacenadas.
+    @GetMapping
+    public List<Solicitud> consultarSolicitudes() {
+        return solicitudService.consultarSolicitudes();
+    }
+
+    //Buscar solicitudes por algun criterio relevante en este caso el ID.
+    @GetMapping("/buscar")
+    public List<Solicitud> buscarSolicitudes(String idSolicitud) {
+        return solicitudService.buscarSolicitudes(idSolicitud);
+    }
+    
+    //Buscar cantidad de solicitudes por fecha // OPERACIÓN ESPECIAL
+    @GetMapping("/contarPorFecha")
+    public int contarSolicitudesPorFecha(String fecha) {
+        return solicitudService.contarSolicitudesPorFecha(fecha);
+    }
+
+    //Registrar una solicitud.
+    @PostMapping("/registrar")
+    public void agregarSolicitud(@RequestBody Solicitud nuevaSolicitud) {
+        solicitudService.agregarSolicitud(nuevaSolicitud);
+    }
+
+    //modificar una solicitud con el id
+    @PostMapping("/modificar")
+    public boolean modificarSolicitud(String idSolicitud, @RequestBody Solicitud solicitudModificada) {
+        return solicitudService.modificarSolicitud(idSolicitud, solicitudModificada);
+    }
+
+    //Eliminar una solicitud por ID
+    @PostMapping("/eliminar")   
+    public boolean eliminarSolicitud(String idSolicitud) {
+        return solicitudService.eliminarSolicitud(idSolicitud);
+    }
+
 
 }
