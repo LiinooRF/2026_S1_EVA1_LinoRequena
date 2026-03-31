3fe433510e77a5c0ef15295102138c801f6f8ec7
LiinooRF <linojotare0704@gmail.com>
2026-03-31 10:37:05 -0300

    first commit

---
+++ linorequena/src/main/java/cl/duoc/linorequena/LinorequenaApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.linorequena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinorequenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinorequenaApplication.class, args);
+	}
+
+}
---
+++ linorequena/src/test/java/cl/duoc/linorequena/LinorequenaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.linorequena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinorequenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
